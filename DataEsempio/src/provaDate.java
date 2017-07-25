26f0b6c129d9fd520c9843c3245dae7e71323083
AndRogJava <rognetta.andrea@yahoo.com>
2017-07-25 13:26:11 +0200

    prova1
    Signed-off-by: AndRogJava <rognetta.andrea@yahoo.com>

---
+++ DataEsempio/src/provaDate.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class provaDate {
+
+	public static void main(String[] args) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date)); //2013/10/15 16:16:39
+
+	}
+
+}
