2d904a2845a7779cf1d83e2b48f9f3a2c8ed29b7
AndRogJava <rognetta.andrea@yahoo.com>
2017-07-25 14:17:00 +0200

    aaa

--- DataEsempio/src/provaDate.java
+++ DataEsempio/src/provaDate.java
@@ -7,7 +7,7 @@ public class provaDate {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 		Date date = new Date();
 		System.out.println(dateFormat.format(date)); //2013/10/15 16:16:39
-
+ c x
 	}
 
 }
